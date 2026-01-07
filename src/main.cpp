f9da57e4385ef19dfe219fa8795a605bfb946236
Filip <filipkazek@gmail.com>
2026-01-07 03:23:39 +0100

    good sampling method

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,15 @@
 #define I2S_SD_OUT  21  
 
 #define I2S_PORT I2S_NUM_0
-#define BUFFER_LEN 256 
+#define BUFFER_LEN 512 
 #define MAX_SAMPLE_LENGHT 22050
-
+#define CHUNK_SIZE 256
 #define INPUT_SHIFT 22
 #define OUTPUT_SHIFT 24
-  const i2s_config_t i2s_config = {
+#define TRIGGER_THRESHOLD 8
+#define SILENCE_LIMIT 3000
+
+const i2s_config_t i2s_config = {
   .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX),
   .sample_rate = 22050, 
   .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
@@ -24,23 +27,27 @@
   .dma_buf_count = 4,
   .dma_buf_len = BUFFER_LEN,
   .use_apll = true
-  };
+};
 
-  const i2s_pin_config_t pin_config = {
+const i2s_pin_config_t pin_config = {
   .bck_io_num = I2S_SCK,
   .ws_io_num = I2S_WS,
   .data_out_num = I2S_SD_OUT, 
   .data_in_num = I2S_SD_IN    
-  };
+};
 
 
 struct Sample{
 int8_t data[MAX_SAMPLE_LENGHT];
-uint16_t lenght;
+uint16_t lenght, playCursor;
+bool isPlaying;
 };
 
 struct Sample Sample1;
 MatrixKeypad keypad;
+
+volatile bool startRecording;
+volatile bool startPlaying;
 
 void AudioTask(void * parameters)
 {
@@ -49,54 +56,88 @@ void AudioTask(void * parameters)
   i2s_zero_dma_buffer(I2S_PORT);
  
   size_t bytesIn,bytesOut;
-  int32_t rawBuffer[BUFFER_LEN];
+  int32_t ioBuffer[CHUNK_SIZE];
+
+  Sample1.isPlaying = false;
+  Sample1.playCursor = 0;
+  
   while(1){
     keypad.update();
-
     if(keypad.isJustPressed('1')){
-      uint16_t totalrecorded = 0;
-      while(totalrecorded < MAX_SAMPLE_LENGHT){
-        i2s_read(I2S_NUM_0,&rawBuffer,sizeof(rawBuffer),&bytesIn,portMAX_DELAY);
+      bool recordingStarted = false;
+      uint16_t totalRecorded = 0;
+      uint16_t silenceCounter = 0;
+      uint16_t lastIndex;
+      Sample1.lenght = 0;
+      while(totalRecorded < MAX_SAMPLE_LENGHT){
+        i2s_read(I2S_NUM_0,&ioBuffer,sizeof(ioBuffer),&bytesIn,portMAX_DELAY);
         if(bytesIn > 0){
           int samplesCount = bytesIn / sizeof(int32_t);
           for(int i=0; i<samplesCount; i++){
-            if (totalrecorded >= MAX_SAMPLE_LENGHT) break;
-            int32_t raw = rawBuffer[i];
+            int32_t raw = ioBuffer[i];
             int32_t processed = raw >> INPUT_SHIFT;
             if (processed > 127) processed = 127;
             if (processed < -128) processed = -128;
-            Sample1.data[totalrecorded] = (int8_t)processed;
-            totalrecorded++;
+
+            if(!recordingStarted){
+              if(abs(processed) > TRIGGER_THRESHOLD)
+                recordingStarted = true;
+            }
+            if(recordingStarted){
+              Sample1.data[totalRecorded] = (int8_t)processed;
+              if(abs(Sample1.data[totalRecorded]) < TRIGGER_THRESHOLD){
+                silenceCounter++;
+              }else{
+                lastIndex = totalRecorded;
+                silenceCounter = 0;
+             }
+            totalRecorded++;
+            if(silenceCounter > SILENCE_LIMIT){
+              Sample1.lenght = lastIndex + 1;
+              goto endrecording;
+            }
+             if (totalRecorded >= MAX_SAMPLE_LENGHT){
+              Sample1.lenght = MAX_SAMPLE_LENGHT;
+              goto endrecording;
+             }
+            }
           }
         }
       }
+      endrecording:
+      ;
     }
 
     if(keypad.isJustPressed('2')) {
-      uint16_t totalplayed = 0;
-      while(totalplayed < MAX_SAMPLE_LENGHT){
-        for(int i=0; i < BUFFER_LEN; i++){
-          if(totalplayed >= MAX_SAMPLE_LENGHT) break;
-          int32_t uncompressed = ((int32_t)Sample1.data[totalplayed] << OUTPUT_SHIFT);
-          rawBuffer[i] = uncompressed;
-          totalplayed++;
-        }
-        i2s_write(I2S_NUM_0,&rawBuffer,sizeof(rawBuffer),&bytesOut,portMAX_DELAY);
+      startPlaying = 0;
+      if(Sample1.lenght > 0){
+        Sample1.isPlaying = true;
+        Sample1.playCursor = 0;
       }
-        memset(rawBuffer, 0, sizeof(rawBuffer));
-        for(int i=0; i < 4; i++) {
-         i2s_write(I2S_NUM_0, &rawBuffer, sizeof(rawBuffer), &bytesOut, portMAX_DELAY);
-      }
-        i2s_zero_dma_buffer(I2S_NUM_0);
     }
-    vTaskDelay(10/portTICK_PERIOD_MS);
+
+    memset(ioBuffer, 0, sizeof(ioBuffer));
+
+    if(Sample1.isPlaying){
+          for(int i=0; i < CHUNK_SIZE; i++){
+            if(Sample1.playCursor < Sample1.lenght){
+              int32_t uncompressed = ((int32_t)Sample1.data[Sample1.playCursor] << OUTPUT_SHIFT);
+              ioBuffer[i] = uncompressed;
+              Sample1.playCursor++;
+            } else {
+              Sample1.isPlaying = false;
+              break;
+            }
+          }
+     }
+  i2s_write(I2S_NUM_0,&ioBuffer,sizeof(ioBuffer),&bytesOut,portMAX_DELAY);
   }
 }
 
 
 void setup() {
   keypad.begin();
-  xTaskCreatePinnedToCore(AudioTask,NULL,4096,NULL,1,NULL,0);
+  xTaskCreatePinnedToCore(AudioTask,NULL,4096,NULL,10,NULL,0);
 }
 
 void loop() {
